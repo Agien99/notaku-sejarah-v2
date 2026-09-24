79a8db91e439c17b67f190a0b0ecb04107e00e61
Agien99 <130525155+Agien99@users.noreply.github.com>
2026-09-24 10:23:01 +0800

    feat: add responsive architecture foundation

---
+++ lib/core/responsive/responsive_layout.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import 'app_breakpoints.dart';
+
+typedef ResponsiveWidgetBuilder =
+    Widget Function(BuildContext context, AppWindowClass windowClass);
+
+class ResponsiveLayout extends StatelessWidget {
+  const ResponsiveLayout({
+    required this.builder,
+    super.key,
+  });
+
+  final ResponsiveWidgetBuilder builder;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final windowClass = AppBreakpoints.fromWidth(constraints.maxWidth);
+
+        return builder(context, windowClass);
+      },
+    );
+  }
+}
+
+extension AppWindowClassX on AppWindowClass {
+  bool get isCompact => this == AppWindowClass.compact;
+
+  bool get isMedium => this == AppWindowClass.medium;
+
+  bool get isExpanded => this == AppWindowClass.expanded;
+
+  bool get usesNavigationRail => !isCompact;
+
+  double get horizontalPagePadding {
+    switch (this) {
+      case AppWindowClass.compact:
+        return 20;
+      case AppWindowClass.medium:
+        return 28;
+      case AppWindowClass.expanded:
+        return 40;
+    }
+  }
+
+  double get maxContentWidth {
+    switch (this) {
+      case AppWindowClass.compact:
+        return 720;
+      case AppWindowClass.medium:
+        return 960;
+      case AppWindowClass.expanded:
+        return 1200;
+    }
+  }
+}
