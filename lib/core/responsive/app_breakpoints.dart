09bbcab0232ac7b4c3ef2caeffa1476b2ad5cbdb
Agien99 <130525155+Agien99@users.noreply.github.com>
2026-09-24 10:22:59 +0800

    feat: add responsive architecture foundation

---
+++ lib/core/responsive/app_breakpoints.dart
@@ -0,0 +1,22 @@
+enum AppWindowClass {
+  compact,
+  medium,
+  expanded,
+}
+
+abstract final class AppBreakpoints {
+  static const double medium = 600;
+  static const double expanded = 900;
+
+  static AppWindowClass fromWidth(double width) {
+    if (width < medium) {
+      return AppWindowClass.compact;
+    }
+
+    if (width < expanded) {
+      return AppWindowClass.medium;
+    }
+
+    return AppWindowClass.expanded;
+  }
+}
