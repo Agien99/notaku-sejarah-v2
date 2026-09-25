f542db6013bdd39b07a42b443006a286a6160448
Agien99 <130525155+Agien99@users.noreply.github.com>
2026-09-25 10:35:19 +0800

    feat(notes): add note chapter model

---
+++ lib/features/notes/domain/models/note_chapter.dart
@@ -0,0 +1,11 @@
+class NoteChapter {
+  const NoteChapter({
+    required this.number,
+    required this.title,
+  });
+
+  final int number;
+  final String title;
+
+  String get label => 'Bab $number';
+}
